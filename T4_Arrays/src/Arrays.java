25b1a5b66e95e08e4e6e57f57b983afb78d13d6e
Agualongo <92918293+AgualongoK@users.noreply.github.com>
2021-11-11 11:30:15 +0100

    Actualización Array

--- T4_Arrays/src/Arrays.java
+++ T4_Arrays/src/Arrays.java
@@ -4,10 +4,18 @@ public class Arrays {
 
         // array de numeros
            int[] numeros = new int[10];
-
-           int[] numerosDos = {1,2,3,4,5,6,7,8,9,10};
-
            boolean[] aciertos = new boolean[5];
            String [] palabras = new String[3];
+           double[] decimales = new double[2];
+        int[] numerosDos = {1,2,3,4,5,6,7,8,9,10};
+        int longitudArray = numerosDos.length;
+        System.out.println(longitudArray);
+
+        // sacar la posición de un array, siempre comienzan desde 0
+        System.out.println(numerosDos[5]);
+
+        for (int i = 0; i <numerosDos.length; i++) {
+            System.out.println(i);
+        }
     }
 }
