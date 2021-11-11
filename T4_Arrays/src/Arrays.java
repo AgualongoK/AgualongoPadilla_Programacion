9a37edbb20cccc51d9499f1d36842b3541d71c68
Agualongo <92918293+AgualongoK@users.noreply.github.com>
2021-11-11 11:11:15 +0100

    Creación clase arrays

---
+++ T4_Arrays/src/Arrays.java
@@ -0,0 +1,10 @@
+public class Arrays {
+
+    public static void main(String[] args) {
+
+        // array de numeros
+           int[] numeros = new int[10];
+
+           int[] numerosDos = {1,2,3,4,5,6,7,8,9,10};
+    }
+}
