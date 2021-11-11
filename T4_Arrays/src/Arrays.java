7795e90f986c75215696dbc16e05a75040df1801
Agualongo <92918293+AgualongoK@users.noreply.github.com>
2021-11-11 11:14:12 +0100

    Update Arrays.java

--- T4_Arrays/src/Arrays.java
+++ T4_Arrays/src/Arrays.java
@@ -6,5 +6,8 @@ public class Arrays {
            int[] numeros = new int[10];
 
            int[] numerosDos = {1,2,3,4,5,6,7,8,9,10};
+
+           boolean[] aciertos = new boolean[5];
+           String [] palabras = new String[3];
     }
 }
