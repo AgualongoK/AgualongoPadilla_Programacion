03a2687206f260aeb580cd23a82c2190cd268df7
Agualongo <92918293+AgualongoK@users.noreply.github.com>
2021-11-22 10:00:35 +0100

    Ejercicio Suma Arias

---
+++ T4_Arrays/src/SumaAristas.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class SumaAristas {
+    public static void main(String[] args) {
+
+        Scanner valor = new Scanner(System.in);
+        int filas, columnas;
+        int sumatorioFila1=0, sumatorioFilaN=0, sumatorioColumna1=0, sumatorioColumnaN=0;
+        int numerosAleatorios[][];
+        System.out.println("Cuántas filas tendrá el array: ");
+        filas = valor.nextInt();
+        System.out.println("Cuántas columnas tendrá el array: ");
+        columnas = valor.nextInt();
+        numerosAleatorios = new int[filas][columnas];
+
+        for (int i = 0; i < numerosAleatorios.length; i++) {
+            //recorro la fila i
+            for (int j = 0; j < numerosAleatorios[i].length; j++) {
+                // recorro la columna j
+                numerosAleatorios[i][j] = (int)(Math.random() * 21);
+                System.out.print(  numerosAleatorios[i][j] + "\t ");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < numerosAleatorios.length; i++) {
+            for (int j = 0; j < numerosAleatorios[i].length; j++) {
+                if (i==0){
+                    sumatorioFila1 += numerosAleatorios[i][j];
+                }
+                if (i==filas-1){
+                    sumatorioFilaN += numerosAleatorios[i][j];
+                }
+                if(j==0){
+                    sumatorioColumna1 += numerosAleatorios[i][j];
+                }
+                if(j==columnas-1){
+                    sumatorioColumnaN += numerosAleatorios[i][j];
+                }
+            }
+        }
+        System.out.println("Suma de la primera fila: " +sumatorioFila1);
+        System.out.println("Suma de la última fila: " +sumatorioFilaN);
+        System.out.println("Suma de la primera columna: " +sumatorioColumna1);
+        System.out.println("Suma de la última columna: " +sumatorioColumnaN);
+    }
+}
